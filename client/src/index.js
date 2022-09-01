8892c6aaf899b2989afd5cab95e73a20d30510df
marwen <marwen.mrabti.me@gmail.com>
2022-09-01 14:21:46 +0100

    fix dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import store from './redux/store';
 import { Provider } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-daterangepicker/daterangepicker.css';
 import './index.styles.scss';
 import App from './App';
 
